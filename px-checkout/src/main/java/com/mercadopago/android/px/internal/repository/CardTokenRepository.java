3c473b2f182629a3a37be4f53ff84195cecc729d
AngieArlanti <angeles.arlanti@mercadolibre.com>
2019-01-28 12:07:54 -0300

    CardTokenRepository's javadocs

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/repository/CardTokenRepository.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/repository/CardTokenRepository.java
@@ -6,5 +6,11 @@ import com.mercadopago.android.px.model.Token;
 
 public interface CardTokenRepository {
 
+    /**
+     * After gathering user card's information, create a cardToken to create Payment.
+     *
+     * @param cardToken: Card information to create cardToken.
+     * @return Token associated to Card.
+     */
     MPCall<Token> createTokenAsync(final CardToken cardToken);
 }
